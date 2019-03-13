bca7f10854a91aab1e09c148ef083003f240613d
qianxinfeng <qianxinfeng@meituan.com>
2019-03-13 17:59:32 +0800

    file

--- spa-app/server/middlewares/staticAssets.js
+++ spa-app/server/middlewares/staticAssets.js
@@ -1,14 +1,15 @@
 const Router=require('koa-router');
 const send = require('koa-send')
+const path = require('path');
 
 module.exports=(root,prefix='/')=>{
   let router=new Router({prefix});
   return router.get('*',async (ctx)=>{
-    let path=ctx.path.replace(prefix,'/');
-    if(/^(\/[\w-]+\/?)*$/.exec(path)||path==='/'){
-      path='/index.html';
+    let newpath=ctx.path.replace(prefix,'/');
+    if(path.extname(newpath)===''){
+      newpath='/index.html';
     }
-    return send(ctx, path,{
+    return send(ctx, newpath,{
       root,
     })
   }).routes();
